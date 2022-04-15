4f90735f495651c7b4be81499bc6e707ab04257c
syy11cn <syy11cn@outlook.com>
2022-04-16 00:59:17 +0800

    feat(parser): module parser

--- packages/parser/src/core.ts
+++ packages/parser/src/core.ts
@@ -1,8 +1,12 @@
 import YAML = require('js-yaml')
-import type {} from '@md-report/types'
 import { isObject } from '@antfu/utils'
+import MarkdownIt = require('markdown-it')
+import type {} from '@md-report/parser'
+import type { ReportConfig, ReportMarkdown } from '@md-report/types'
 
-export function matter(code: string) {
+const md = MarkdownIt({ html: true })
+
+export function matter(code: string): { data: ReportConfig; content: string } {
   let data: any = {}
   const content = code.replace(/^---.*\r?\n([\s\S]*?)---/,
     (_, d) => {
@@ -14,5 +18,12 @@ export function matter(code: string) {
   return { data, content }
 }
 
-// export function parse(markdown: string, filepath?: string) {
-// }
+export function parse(markdown: string): ReportMarkdown {
+  const { data: frontmatter, content: rawContent } = matter(markdown)
+  const content = md.parse(rawContent, null)
+  return {
+    raw: markdown,
+    frontmatter,
+    content,
+  }
+}
--- packages/parser/src/fs.ts
+++ packages/parser/src/fs.ts
@@ -1,14 +1,8 @@
-/* eslint-disable no-console */
 import { promises as fs } from 'fs'
 import type { ReportMarkdown } from '@md-report/types'
-// import { dirname, resolve } from 'path'
-// import { parse } from './core'
+import { parse } from './core'
 
 export async function load(filepath: string, content?: string): Promise<ReportMarkdown> {
   const markdown = content ?? await fs.readFile(filepath, 'utf8')
-
-  return {
-    raw: markdown,
-    frontmatter: { title: '111' },
-  }
+  return parse(markdown)
 }
